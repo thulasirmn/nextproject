8727961bffebfd1dbca27a876c85db8cc2f3cb76
Thulasiraman <thulasirmn8@gmail.com>
2023-12-28 22:56:21 +0530

    Add pricing cards and update styles

--- app/components/PricingCards.tsx
+++ app/components/PricingCards.tsx
@@ -27,9 +27,120 @@ const PricingCards = (props: Props) => {
                         >Purchase</a>
                     </div>
                 </div>
+                {/* Border */}
+                <div className='border-t border-slate-700'></div>
+                {/* Lower container */}
+                <div className='bg-slate-800 p-8 mx-3 mb-3 rounded-b-xl'>
+                    <div className='flex flex-col space-y-2'>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>Item</div>
-            <div>Item</div>
+
+            <div className='bg-slate-700 rounded-xl text-white'>
+                {/* Upper container */}
+                <div className='p-8 mx-3 mt-3 rounded-t-xl bg-slate-800'>
+                    <div>Basic</div>
+                    <h2 className='mt-10 font-serif text-5xl text-center'>99GB</h2>
+                    <h3 className='mt-2 text-center'>$1.99/Month</h3>
+                    <div className='
+                        flex justify-center
+                    '>
+                        <a href="#"
+                        className='
+                            inline-block px-10 py-3 my-6 text-center border border-violet-600
+                            rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800
+                        '
+                        >Purchase</a>
+                    </div>
+                </div>
+                {/* Border */}
+                <div className='border-t border-slate-700'></div>
+                {/* Lower container */}
+                <div className='bg-slate-800 p-8 mx-3 mb-3 rounded-b-xl'>
+                    <div className='flex flex-col space-y-2'>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='bg-slate-700 rounded-xl text-white'>
+                {/* Upper container */}
+                <div className='p-8 mx-3 mt-3 rounded-t-xl bg-slate-800'>
+                    <div>Basic</div>
+                    <h2 className='mt-10 font-serif text-5xl text-center'>99GB</h2>
+                    <h3 className='mt-2 text-center'>$1.99/Month</h3>
+                    <div className='
+                        flex justify-center
+                    '>
+                        <a href="#"
+                        className='
+                            inline-block px-10 py-3 my-6 text-center border border-violet-600
+                            rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800
+                        '
+                        >Purchase</a>
+                    </div>
+                </div>
+                {/* Border */}
+                <div className='border-t border-slate-700'></div>
+                {/* Lower container */}
+                <div className='bg-slate-800 p-8 mx-3 mb-3 rounded-b-xl'>
+                    <div className='flex flex-col space-y-2'>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                        <div className='flex justify-center'>
+                              <svg className = "text-sm"xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                  <polyline points="20 6 9 17 4 12"></polyline>
+                                </svg>
+                                <span className='ml-1 text-sm'>100GB of storage</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
